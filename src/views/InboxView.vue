2bc139b5fef8ab724c1fab20ef1b261ea650a4e5
Henning 'haenno' Beier <haenno@web.de>
2023-08-20 12:23:09 +0200

    try pagnation

--- src/views/InboxView.vue
+++ src/views/InboxView.vue
@@ -1,15 +1,19 @@
 <template>
   <div>inbox view</div>
+  <Pagination v-model="currentPage" :total-pages="100" :slice-length="4"></Pagination>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import { Pagination } from 'flowbite-vue'
+import { ref } from 'vue'
+
+
 export default defineComponent({
   setup() {
-    return {}
+    const currentPage = ref(1)
+    return  { currentPage, Pagination }
   }
 })
 </script>
-
-<style scoped></style>
