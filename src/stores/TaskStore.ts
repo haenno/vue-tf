b0fa58eec34cc234636ab48e7d54d0547295a9f3
haenno <haenno@web.de>
2023-08-21 23:34:21 +0200

    add tasks

--- src/stores/TaskStore.ts
+++ src/stores/TaskStore.ts
@@ -2,10 +2,12 @@ import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('taskstore', {
   state: () => ({
-    tasks: [
-      { id: 1, title: 'Task 1', isFav: false },
-      { id: 2, title: 'Task 2', isFav: true }
-    ],
+    tasks: [],
+    isLoading: false,
+    //tasks: [
+//      { id: 1, title: 'Task 1', isFav: false },
+      //{ id: 2, title: 'Task 2', isFav: true }
+    //],
     name: 'Fancy Task Store'
   }),
   getters: {
@@ -22,21 +24,83 @@ export const useTaskStore = defineStore('taskstore', {
     }
   },
   actions: {
-    addTask(task: { id: number; title: string; isFav: boolean }) {
-      this.tasks.push(task)
+    async getTasks() {
+      this.isLoading = true
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkyNjUzNjQwLCJpYXQiOjE2OTI2NTMzNDAsImp0aSI6ImM5MDU2NGM3Mzc2OTQ3Zjc5ZWI5YTE5MDUyODViMWE3IiwidXNlcl9pZCI6MX0.Uz8t_1W1s-42dSZ7P57ryW90Pig3UUAk7KYgIV8IY2E');
+      myHeaders.append('Content-Type', 'application/json');
+
+
+      
+      const res = await fetch("https://drf-jwt.tstsrv.de/tasks_test_api/task/", { headers: myHeaders })
+      const data = await res.json()
+      this.tasks = data
+      this.isLoading = false
     },
-    deleteTask(id: number) {
+
+
+      async addTask(task: { id: number; title: string; isFav: boolean }) {
+        
+        this.isLoading = true
+        const myHeaders = new Headers();
+        myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkyNjUzNjQwLCJpYXQiOjE2OTI2NTMzNDAsImp0aSI6ImM5MDU2NGM3Mzc2OTQ3Zjc5ZWI5YTE5MDUyODViMWE3IiwidXNlcl9pZCI6MX0.Uz8t_1W1s-42dSZ7P57ryW90Pig3UUAk7KYgIV8IY2E');
+        
+        this.tasks.push(task)
+      const res = await fetch("https://drf-jwt.tstsrv.de/tasks_test_api/task/", {
+         method:"POST",
+         body: JSON.stringify(task), 
+         headers: myHeaders 
+        })
+
+        if(res.error) {
+          console.log(res.error)
+         }
+         this.isLoading = false         
+      },
+
+    async deleteTask(id: number) {
+      this.isLoading = true
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkyNjUzNjQwLCJpYXQiOjE2OTI2NTMzNDAsImp0aSI6ImM5MDU2NGM3Mzc2OTQ3Zjc5ZWI5YTE5MDUyODViMWE3IiwidXNlcl9pZCI6MX0.Uz8t_1W1s-42dSZ7P57ryW90Pig3UUAk7KYgIV8IY2E');
+
+
       this.tasks = this.tasks.filter((t: { id: number; title: string; isFav: boolean }) => {
         return t.id !== id
       })
+
+      const res = await fetch("https://drf-jwt.tstsrv.de/tasks_test_api/task/" + id, {
+         method:"DELETE",
+         headers: myHeaders          
+        })
+
+        if(res.error) {
+          console.log(res.error)
+         }      
+         this.isLoading = false
     },
-    toggleFav(id: number) {
+    async toggleFav(id: number) {
+      this.isLoading = true
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkyNjUzNjQwLCJpYXQiOjE2OTI2NTMzNDAsImp0aSI6ImM5MDU2NGM3Mzc2OTQ3Zjc5ZWI5YTE5MDUyODViMWE3IiwidXNlcl9pZCI6MX0.Uz8t_1W1s-42dSZ7P57ryW90Pig3UUAk7KYgIV8IY2E');
+
+
       const task = this.tasks.find(
         (t: { id: number; title: string; isFav: boolean }) => t.id === id
       )
       if (task) {
         task.isFav = !task.isFav
       }
+
+      const res = await fetch("https://drf-jwt.tstsrv.de/tasks_test_api/task/" + id, {
+        method:"PATCH",
+        body: JSON.stringify( {isFav: task.isFav } ), 
+        headers: myHeaders 
+       })
+
+       if(res.error) {
+         console.log(res.error)
+        }
+        this.isLoading = false
     }
   }
 })
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -9,7 +9,11 @@
     <tabs v-model="activeTab" class="p-5">
       <!-- class appends to content DIV for all tabs -->
       <tab @click="filter = 'all'" name="all" title="All tasks">
-        <div v-if="filter === 'all'">
+        
+        <spinner v-if="taskStore.isLoading" class="m-5" color="yellow" size="10" />
+
+
+        <div v-if="filter === 'all'" >
           <!-- return here: https://www.youtube.com/watch?v=ixxSKJi4QXI&list=PL4cUxeGkcC9hp28dYyYBy3xoOdoeNw-hD&index=8 for async actions -->
 
           <p>All tasks: (You have {{ taskStore.totalCount }} Tasks)</p>
@@ -20,6 +24,9 @@
         </div>
       </tab>
       <tab @click="filter = 'favs'" name="favs" title="Favorites">
+
+
+        <spinner v-if="taskStore.isLoading" class="m-5" color="yellow" size="10" />        
         <div v-if="filter === 'favs'">
           <p>Favorite tasks: (You have {{ taskStore.favCount }} Favorites )</p>
           <div v-for="task in taskStore.favs" :key="task.id">
@@ -37,17 +44,22 @@ import { useTaskStore } from '@/stores/TaskStore'
 import TaskDetails from '@/components/TaskDetails.vue'
 import TaskForm from '@/components/TaskForm.vue'
 import { Tabs, Tab } from 'flowbite-vue'
+import { Spinner } from 'flowbite-vue'
 
 export default defineComponent({
   components: {
     TaskDetails,
     TaskForm,
     Tabs,
-    Tab
+    Tab,
+    Spinner
   },
   setup() {
     const activeTab = ref('all')
     const taskStore = useTaskStore()
+
+    // fetch tasks
+    taskStore.getTasks()
 
     // const filter = ref('all')
     const filter = activeTab
