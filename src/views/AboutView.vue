4c7ea1972c9cab6a6dfead76d5b244da9a5556a5
haenno <haenno@web.de>
2023-08-25 08:55:40 +0200

    add and test @heroicons/vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { useCounterStore } from '@/stores/counter'
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+import { Battery0Icon } from '@heroicons/vue/24/solid'
+import { Battery50Icon } from '@heroicons/vue/24/outline'
+import { Battery100Icon } from '@heroicons/vue/24/solid'
 
 const counter = useCounterStore()
 
@@ -12,4 +16,10 @@ console.log(counter.count)
 
 <template>
   <h1>This is an about page</h1>
+  <div id="icon_test">
+    <BeakerIcon class="h-16 w-16 text-blue-500" />
+    <Battery0Icon class="h-12 w-12 text-red-500" />
+    <Battery50Icon class="h-8 w-8 text-yellow-300" />
+    <Battery100Icon class="h-24 w-24 text-green-500" />
+  </div>
 </template>
