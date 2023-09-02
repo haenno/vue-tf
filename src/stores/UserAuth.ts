2e1bfa266b844a09c5b4b9e0dee7d4de9f4772ee
haenno <haenno@web.de>
2023-09-02 13:52:54 +0200

    add cookies for token storage

--- src/stores/UserAuth.ts
+++ src/stores/UserAuth.ts
@@ -1,6 +1,10 @@
 import { defineStore } from 'pinia'
 import { useTaskStore } from '@/stores/TaskStore'
 
+import Cookies from 'js-cookie'
+
+const authCookie = Cookies.withAttributes({ sameSite: 'strict', secure: true })
+
 const authUrl = 'https://drf-jwt.tstsrv.de/api/token/'
 
 const myHeaders = new Headers()
@@ -34,33 +38,70 @@ export const useUserAuthStore = defineStore('userauthstore', {
       console.log(response)
       if (response.ok) {
         const data = await response.json()
+
         this.authToken = data.access
+        authCookie.set('authToken', this.authToken)
         sessionStorage.setItem('authToken', this.authToken)
+
         this.refreshToken = data.refresh
+        authCookie.set('refreshToken', this.refreshToken)
         sessionStorage.setItem('refreshToken', this.refreshToken)
+
         this.isLoggedIn = true
       } else {
         this.authToken = ''
+        authCookie.remove('authToken')
         sessionStorage.removeItem('authToken')
+
         this.refreshToken = ''
+        authCookie.remove('refreshToken')
         sessionStorage.removeItem('refreshToken')
+
         this.isLoggedIn = false
       }
     },
     async logout() {
+      this.authToken = ''
+      sessionStorage.removeItem('authToken')
+      authCookie.remove('authToken')
+
+      this.refreshToken = ''
+      sessionStorage.removeItem('refreshToken')
+      authCookie.remove('refreshToken')
+
       this.isLoggedIn = false
     },
     async init() {
+      this.isLoggedIn = false
+
       if (sessionStorage.getItem('refreshToken') && sessionStorage.getItem('authToken')) {
         this.authToken = sessionStorage.getItem('authToken')!
+        authCookie.set('authToken', this.authToken)
+
         this.refreshToken = sessionStorage.getItem('refreshToken')!
+        authCookie.set('refreshToken', this.refreshToken)
+
         this.isLoggedIn = true
-      } else {
+      }
+
+      if (authCookie.get('refreshToken') && authCookie.get('authToken')) {
+        this.authToken = authCookie.get('authToken')!
+        sessionStorage.setItem('authToken', this.authToken)
+
+        this.refreshToken = authCookie.get('refreshToken')!
+        sessionStorage.setItem('refreshToken', this.refreshToken)
+
+        this.isLoggedIn = true
+      }
+
+      if (this.isLoggedIn === false) {
         this.authToken = ''
         sessionStorage.removeItem('authToken')
+        authCookie.remove('authToken')
+
         this.refreshToken = ''
         sessionStorage.removeItem('refreshToken')
-        this.isLoggedIn = false
+        authCookie.remove('refreshToken')
       }
     }
   }
