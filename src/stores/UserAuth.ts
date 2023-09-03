742b2ac2a89aa2648b53741e88713559ad7dd8da
haenno <haenno@web.de>
2023-09-04 00:14:01 +0200

    activate access token validation and refresh

--- src/stores/UserAuth.ts
+++ src/stores/UserAuth.ts
@@ -57,23 +57,33 @@ export const useUserAuthStore = defineStore('userauthstore', {
 
         this.isLoggedIn = true
       } else {
-        this.authToken = ''
-        authCookie.remove('authToken')
-
-        this.refreshToken = ''
-        authCookie.remove('refreshToken')
-
-        this.isLoggedIn = false
+        this.logout()
       }
     },
     async logout() {
+      this.isLoggedIn = false
+
+      // blacklist refresh token
+      let token_to_blacklist = null
+      if (this.refreshToken) {
+        token_to_blacklist = this.refreshToken
+      }
+      if (authCookie.get('refreshToken')) {
+        token_to_blacklist = authCookie.get('refreshToken')!
+      }
+      if (token_to_blacklist != null) {
+        await fetch(blacklistTokenUrl, {
+          body: JSON.stringify({ refresh: token_to_blacklist }),
+          headers: myHeaders,
+          method: 'POST'
+        })
+      }
+
       this.authToken = ''
       authCookie.remove('authToken')
 
       this.refreshToken = ''
       authCookie.remove('refreshToken')
-
-      this.isLoggedIn = false
     },
     async init() {
       this.isLoggedIn = false
@@ -85,19 +95,51 @@ export const useUserAuthStore = defineStore('userauthstore', {
       }
 
       if (this.isLoggedIn === false) {
-        this.authToken = ''
-        authCookie.remove('authToken')
-
-        this.refreshToken = ''
-        authCookie.remove('refreshToken')
+        this.logout()
       }
     },
     async validateTokens() {
-      console.log('validateTokens: start')
       if (this.isLoggedIn) {
-        console.log('validateTokens: user is logged in')
-      } else {
-        console.log('validateTokens: user is NOT logged in')
+        // check if refresh token is valid
+        const response_refreshTokenVerify = await fetch(verifyTokenUrl, {
+          body: JSON.stringify({ token: this.refreshToken }),
+          headers: myHeaders,
+          method: 'POST'
+        })
+
+        if (!response_refreshTokenVerify.ok) {
+          console.log('validateTokens: refreshToken is NOT valid')
+          this.logout()
+          return
+        }
+
+        // check if access token is valid
+        const response_authTokenVerify = await fetch(verifyTokenUrl, {
+          body: JSON.stringify({ token: this.authToken }),
+          headers: myHeaders,
+          method: 'POST'
+        })
+
+        if (!response_authTokenVerify.ok) {
+          // if access token is expired, refresh it by using the refresh token
+          const response_refreshToken = await fetch(refreshTokenUrl, {
+            body: JSON.stringify({ refresh: this.refreshToken }),
+            headers: myHeaders,
+            method: 'POST'
+          })
+
+          if (response_refreshToken.ok) {
+            const data = await response_refreshToken.json()
+            this.authToken = data.access
+            authCookie.set('authToken', this.authToken)
+            this.refreshToken = data.refresh
+            authCookie.set('refreshToken', this.refreshToken)
+          } else {
+            console.log('validateTokens: access token refresh did NOT work!')
+            this.logout()
+            return
+          }
+        }
       }
     }
   }
