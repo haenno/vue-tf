75454fdc41663e8c409b3272f4d23e65131b967f
haenno <haenno@web.de>
2023-09-09 16:46:26 +0200

    next try fix workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const BASE_PATH = process.env.BASE_PATH || ''
+const BASE_PATH = process.env.BASE_PATH || '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
